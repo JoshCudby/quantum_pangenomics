ec2bffe127dc6412f3ab330e06316441ab605bf9
Josh Cudby <jjcc2@cam.ac.uk>
2024-08-23 13:58:22 +0100

    Update oriented model

---
+++ bin/oriented_tangle/build_qubo_input.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+usage()
+{
+    echo "usage: build_qubo_input [[[-f file] [-n normalisation] [-m memory]] | [-h]]"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                filename="$1"
+                                ;;
+        -n | --normalisation )  shift
+                                normalisation="$1"
+                                ;;
+        -m | --memory )         shift
+                                memory="$1"
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+if [ -f "./data/"$filename ]; then
+    echo "Reading file:" $filename
+else
+    echo "Could not find input file."
+    exit 1
+fi
+
+case $normalisation in
+    [0-9]* ) echo "Normalising node weights by:" $normalisation
+             ;;
+    *      ) echo "Normalisation was not a number."; exit 1
+esac
+
+case $memory in
+    [0-9]* ) echo "Memory:" $memory
+             ;;
+    *      ) echo "Memory was not a number."; exit 1
+esac
+
+
+## MAIN
+bsub -J  "build_qubo" -R '"select[mem>'$memory'] rusage[mem='$memory']"' -M "$memory" -o "out/oriented/build.$filename.%J.%I" -e "out/oriented/error.build.$filename.%J" -G "qpg" "python3 ./oriented_tangle/build_oriented_qubo_matrix.py $filename $normalisation"
