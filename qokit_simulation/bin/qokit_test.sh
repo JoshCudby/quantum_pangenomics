a51d2d17c4ab28adbde566bc3253236c82320968
Josh Cudby <jjcc2@cam.ac.uk>
2024-10-24 15:54:29 +0100

    QOKit job updates

--- qokit_simulation/bin/qokit_test.sh
+++ qokit_simulation/bin/qokit_test.sh
@@ -7,6 +7,6 @@ memory=2000
 mkdir -p out/qokit
 # QOKit Testing
 echo "QOKit Testing"
-bsub -R '"select[mem>'$memory'] rusage[mem='$memory']"' -M "$memory" -o "out/qokit.test.%J" -e "out/error.qokit.test.%J" -G "qpg" -q "qpg" "python3 ./qokit_job_testing.py"
+bsub -R '"select[mem>'$memory'] rusage[mem='$memory']"' -gpu - -M "$memory" -o "out/qokit.test.%J" -e "out/error.qokit.test.%J" -G "qpg" -q "qpg" "python3 ./qokit_job_testing.py"
 
 exit 0
