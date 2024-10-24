b329a58e8b47b75c281021bd3d91f1552ca45af7
Josh Cudby <jjcc2@cam.ac.uk>
2024-10-24 16:00:58 +0100

    QOKit job updates

--- qokit_simulation/bin/qokit_test.sh
+++ qokit_simulation/bin/qokit_test.sh
@@ -5,8 +5,8 @@
 memory=2000
 
 module load cuda-12.1.1
-WORKING_DIR=/nfs/users/nfs_j/jc59/qgenomics/pagnenome/qokit_simulation
-source /nfs/users/nfs_j/jc59/qokit_311/bin/activate
+WORKING_DIR=/nfs/users/nfs_j/jc59/quantumwork/pangenome/qokit_simulation
+source ~/qokit_311/bin/activate
 
 # QOKit Testing
 echo "QOKit Testing"
