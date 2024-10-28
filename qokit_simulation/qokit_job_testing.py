0dcf6a834cd88ade6ae769198cae3000a5b3e320
Josh Cudby <jjcc2@cam.ac.uk>
2024-10-28 11:19:37 +0000

    QOKit job updates

--- qokit_simulation/qokit_job_testing.py
+++ qokit_simulation/qokit_job_testing.py
@@ -1,50 +1,40 @@
 import numpy as np
-from itertools import combinations, combinations_with_replacement
 import scipy
 import matplotlib.pyplot as plt
-
-from qokit.utils import brute_force
-from qokit import get_qaoa_objective
 from qokit.fur import choose_simulator, get_available_simulator_names
+from qokit import parameter_utils
+from qokit.qaoa_objective import get_qaoa_objective
+
+
+rng = np.random.default_rng(10)
 
 data = np.load('../qubo_solvers/out/tangle/qubo_data_test.npy', allow_pickle=True)
 Q, offset, W = data
 # Move terms to upper triangular part
 Q = np.triu(Q) * 2
 Q -= np.triu(np.triu(Q).T) / 2
-N_vars = Q.shape[0]
-print(N_vars)
+N = Q.shape[0]
+print(N)
 
 # Get Hamiltonian terms
-terms = [(Q[i, j], [i, j]) for i in range(N_vars) for j in range(i, N_vars)]
+terms = [(Q[i, j], [i, j]) for i in range(N) for j in range(i, N)]
 
 
 print(get_available_simulator_names("x"))
 simclass = choose_simulator(name='auto')
-sim = simclass(N_vars, terms=terms)
+sim = simclass(N, terms=terms)
 print(type(sim))
 sim.get_cost_diagonal()
 
-p = 3
-gamma, beta = np.random.rand(2, 3)
-_result = sim.simulate_qaoa(gamma, beta) # Result depends on the type of simulator. 
-sv = sim.get_statevector(_result)
-print(sv)
-probs = sim.get_probabilities(_result)
-probs.sum()
+# Initial
+p = 1
+gamma, beta = rng.random((2, 3))
+u, v = parameter_utils.to_fourier_basis(gamma, beta)
+init_freq = np.hstack([u, v])
 
-probs = sim.get_probabilities(_result, preserve_state=False)
-sv2 = sim.get_statevector(_result)
-print("Using numpy") if np.allclose(sv, sv2) else print("Yohoo, I'm using a memory-economic simulator!")
+f = get_qaoa_objective(N, p, parameterization='freq', objective='overlap')
+print(f"Success probability at p={p} before optimization is {1-f(init_freq)}")
 
-overlap = sim.get_overlap(_result)
-print("Ground state overlap:", overlap)
-# Below we test that for positive-valued cost function, the maximum can be achieved 
-# by either inverting the values, or negating the values.
-costs_abs = np.abs(sim.get_cost_diagonal())
-print("Overlap with ground state for absolute cost:", sim.get_overlap(_result, costs=costs_abs))
-overlap_inv = sim.get_overlap(_result, costs=1/costs_abs)
-print("Overlap with highest state (inverted costs):", overlap_inv)
-overlap_neg = sim.get_overlap(_result, costs=-costs_abs)
-print("Overlap with highest state (negative):", overlap_neg)
-assert overlap_inv == overlap_neg, "You may have values of mixed sign in your cost."
+res = scipy.optimize.minimize(f, init_freq, method='COBYLA', options={'rhobeg': 0.01/N})
+u_opt, v_opt = res.x[:p], res.x[p:]
+print(f"Success probability at p={p} after optimization is {1-f(np.hstack([u_opt, v_opt]))}")
