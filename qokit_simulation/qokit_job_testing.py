28671b6b6a3ed5616b629e942a404f5552949ee0
Josh Cudby <jjcc2@cam.ac.uk>
2024-10-28 09:43:35 +0000

    QOKit job updates

--- qokit_simulation/qokit_job_testing.py
+++ qokit_simulation/qokit_job_testing.py
@@ -12,20 +12,39 @@ N_vars = 4
 np.random.seed(100)
 terms = [(np.random.normal(), spin_pair) for spin_pair in combinations_with_replacement(range(N_vars), r=2)]
 
-print(get_available_simulator_names("x"))
-simclass = choose_simulator(name='auto')
-sim = simclass(N_vars, terms=terms)
-sim.get_cost_diagonal()
+# print(get_available_simulator_names("x"))
+# simclass = choose_simulator(name='auto')
+# sim = simclass(N_vars, terms=terms)
+# sim.get_cost_diagonal()
 
-p = 3
-gamma, beta = np.random.rand(2, 3)
-_result = sim.simulate_qaoa(gamma, beta) # Result depends on the type of simulator. 
-sv = sim.get_statevector(_result)
-print(sv)
-probs = sim.get_probabilities(_result)
-probs.sum()
+# p = 3
+# gamma, beta = np.random.rand(2, 3)
+# _result = sim.simulate_qaoa(gamma, beta) # Result depends on the type of simulator. 
+# sv = sim.get_statevector(_result)
+# print(sv)
+# probs = sim.get_probabilities(_result)
+# probs.sum()
 
-probs = sim.get_probabilities(_result, preserve_state=False)
-sv2 = sim.get_statevector(_result)
-print("Using numpy") if np.allclose(sv, sv2) else print("Yohoo, I'm using a memory-economic simulator!")
-print(sv2)
+# probs = sim.get_probabilities(_result, preserve_state=False)
+# sv2 = sim.get_statevector(_result)
+# print("Using numpy") if np.allclose(sv, sv2) else print("Yohoo, I'm using a memory-economic simulator!")
+# print(sv2)
+
+# Get objective
+p = 5
+f = get_qaoa_objective(N_vars, p, terms=terms, parameterization='theta')
+initial_gamma = -1*np.linspace(0, 1, p)
+initial_beta = np.linspace(1, 0, p)
+
+res = scipy.optimize.minimize(f, np.hstack([initial_gamma, initial_beta]), method='COBYLA', options={'rhobeg': 0.01})
+print(f"Expected QAOA solution quality: {res.fun:.5f}")
+
+def f_from_terms_ground_truth(s):
+    """ground truth for debugging
+    """
+    out = 0
+    for coeff, (i, j) in terms:
+        out += coeff * s[i] * s[j]
+    return out
+
+print(f"True minimum: {brute_force(f_from_terms_ground_truth, N_vars, minimize=True)[0]:.5f}")
