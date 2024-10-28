d2b589e8b29648724ab9d5858ec21b7dfd0fafd0
Josh Cudby <jjcc2@cam.ac.uk>
2024-10-28 10:59:56 +0000

    QOKit job updates

--- qokit_simulation/qokit_job_testing.py
+++ qokit_simulation/qokit_job_testing.py
@@ -5,53 +5,34 @@ import matplotlib.pyplot as plt
 
 from qokit.utils import brute_force
 from qokit import get_qaoa_objective
-# from qokit.fur import choose_simulator, get_available_simulator_names
+from qokit.fur import choose_simulator, get_available_simulator_names
 
-# Small test
-N_vars = 4
-rng = np.random.default_rng(10)
-terms = [(rng.normal(), spin_pair) for spin_pair in combinations_with_replacement(range(N_vars), r=2)]
+data = np.load('../qubo_solvers/out/tangle/qubo_data_test.npy', allow_pickle=True)
+Q, offset, W = data
+# Move terms to upper triangular part
+Q = np.triu(Q) * 2
+Q -= np.triu(np.triu(Q).T) / 2
+N_vars = Q.shape[0]
 
-# print(get_available_simulator_names("x"))
-# simclass = choose_simulator(name='auto')
-# sim = simclass(N_vars, terms=terms)
-# sim.get_cost_diagonal()
+# Get Hamiltonian terms
+terms = [(Q[i, j], [i, j]) for i in range(N_vars) for j in range(i, N_vars)]
 
-# p = 3
-# gamma, beta = np.random.rand(2, 3)
-# _result = sim.simulate_qaoa(gamma, beta) # Result depends on the type of simulator. 
-# sv = sim.get_statevector(_result)
-# print(sv)
-# probs = sim.get_probabilities(_result)
-# probs.sum()
 
-# probs = sim.get_probabilities(_result, preserve_state=False)
-# sv2 = sim.get_statevector(_result)
-# print("Using numpy") if np.allclose(sv, sv2) else print("Yohoo, I'm using a memory-economic simulator!")
-# print(sv2)
+print(get_available_simulator_names("x"))
+simclass = choose_simulator(name='auto')
+sim = simclass(N_vars, terms=terms)
+print(type(sim))
+sim.get_cost_diagonal()
 
-# Get objective
-p = 5
-f = get_qaoa_objective(N_vars, p, terms=terms, parameterization='theta', objective='expectation')
-initial_gamma = -1*np.linspace(0, 1, p)
-initial_beta = np.linspace(1, 0, p)
+p = 3
+gamma, beta = np.random.rand(2, 3)
+_result = sim.simulate_qaoa(gamma, beta) # Result depends on the type of simulator. 
+sv = sim.get_statevector(_result)
+print(sv)
+probs = sim.get_probabilities(_result)
+probs.sum()
 
-res = scipy.optimize.minimize(f, np.hstack([initial_gamma, initial_beta]), method='COBYLA', options={'rhobeg': 0.01})
-print(f"Expected QAOA solution quality: {res.fun:.5f}")
-print(f"Optimised parameters: {res.x}")
-
-def f_from_terms_ground_truth(s):
-    """ground truth for debugging
-    """
-    out = 0
-    for coeff, (i, j) in terms:
-        out += coeff * s[i] * s[j]
-    return out
-
-min, sol = brute_force(f_from_terms_ground_truth, N_vars, minimize=True)
-print(f"True minimum: {min}")
-print(f"True sol: {sol}")
-
-print(f_from_terms_ground_truth([1] * N_vars))
-print(f_from_terms_ground_truth([-1] * N_vars))
-print(f_from_terms_ground_truth([-1,-1,-1,1]))
+probs = sim.get_probabilities(_result, preserve_state=False)
+sv2 = sim.get_statevector(_result)
+print("Using numpy") if np.allclose(sv, sv2) else print("Yohoo, I'm using a memory-economic simulator!")
+print(sv2)
