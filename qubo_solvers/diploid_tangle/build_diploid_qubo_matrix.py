85e037aac0ae8f46252cbeb5934c94e700c4e110
Josh Cudby <jjcc2@cam.ac.uk>
2024-10-29 12:22:09 +0000

    Refactor data handling

--- qubo_solvers/diploid_tangle/build_diploid_qubo_matrix.py
+++ qubo_solvers/diploid_tangle/build_diploid_qubo_matrix.py
@@ -1,13 +1,14 @@
 import sys
 import numpy as np
+import os
 from pathlib import Path
 from utils.graph_utils import oriented_graph_from_file, normalise_node_weights
 from utils.qubo_utils import qubo_matrix_from_graph
 
 if len(sys.argv) > 1:
-    filename = sys.argv[1]
+    filepath = sys.argv[1]
 else:
-    filename = "test.gfa"
+    filepath = "data/test.gfa"
 
 if len(sys.argv) > 2:
     try:
@@ -17,24 +18,25 @@ if len(sys.argv) > 2:
 else:
     normalisation = 1
    
+filename = os.path.basename(filepath)
 
-graph = oriented_graph_from_file(f"../data/{filename}")
+graph = oriented_graph_from_file(filepath)
 print(f'Normalising by {normalisation}')
 graph = normalise_node_weights(graph, normalisation)
 qubo_matrix, offset, T_max, N = qubo_matrix_from_graph(graph)
 
-path='out/diploid'
-Path(path).mkdir(exist_ok=True)
+out_path='out/diploid'
+Path(out_path).mkdir(exist_ok=True)
 to_save = np.array([qubo_matrix, offset, T_max, N], dtype=object)
-filepath = f'{path}/qubo_data_{filename}_normalisation_{normalisation}'
-np.save(filepath, to_save)
+np_data_filepath = f'{out_path}/qubo_data_{filename}_normalisation_{normalisation}'
+np.save(np_data_filepath, to_save)
 
 # Write to MQLib Format
-filepath = f'out/diploid/mqlib_input_{filename}_normalisation_{normalisation}.txt'
+mqlib_data_filepath = f'out/diploid/mqlib_input_{filename}_normalisation_{normalisation}.txt'
 ut_qubo_matrix = np.triu(qubo_matrix)
 non_zero = np.nonzero(ut_qubo_matrix)
 non_zero_count = int(non_zero[0].shape[0])
-f = open(filepath, 'w')
+f = open(mqlib_data_filepath, 'w')
 f.write(f'{qubo_matrix.shape[0]} {non_zero_count}\n')
 to_write = ''
 for i in range(len(non_zero[0])):
