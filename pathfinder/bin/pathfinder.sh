edc00907eae2212c14be91bdf251c98b8f79adf0
Josh Cudby <jjcc2@cam.ac.uk>
2024-12-10 13:27:15 +0000

    Pathfinder script changes

--- pathfinder/bin/pathfinder.sh
+++ pathfinder/bin/pathfinder.sh
@@ -3,10 +3,10 @@
 filepath="$1"
 filename=$(basename -- "$filepath")
 
-rm ./out/$filename.txt
-rm ./out/error.$filename.txt
+rm $SCRATCH/out/pathfinder/$filename.txt
+rm $SCRATCH/out/pathfinder/error.$filename.txt
 
-bsub -o ./out/$filename.txt -e ./out/error.$filename.txt\
+bsub -o $SCRATCH/out/pathfinder/$filename.txt -e $SCRATCH/out/pathfinder/error.$filename.txt\
  -n 4\
  -R "select[mem>32000] rusage[mem=32000]" -M 32000 -q qpg -gpu -\
  "/lustre/scratch127/qpg/cz3/QuantumTangle/pathfinder/pathfinder $filepath -N 100000000"
