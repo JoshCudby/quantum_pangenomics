d7d3c22ecce84b3d6968d77369c4856f50f8adc5
Josh Cudby <jjcc2@cam.ac.uk>
2025-02-20 17:02:29 +0000

    Scripts for cvar qaoa

---
+++ qiskit_simulation/bin/optimize_cvar.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+usage()
+{
+    echo "usage: optimize_cvar.sh [[-f file -m memory -p reps] | [-h]]"
+}
+
+memory=4000
+reps=4
+num_gpu=1
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                filename="$1"
+                                ;;
+        -m | --memory )         shift
+                                memory="$1"
+                                ;;
+        -p | --reps )           shift
+                                reps="$1"
+                                ;;
+        -n | --shots )          shift
+                                shots="$1"
+                                ;;
+        -g | --num-gpu )        shift
+                                num_gpu="$1"
+                                ;;
+        --hardware )            hardware="--hardware"
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+## MAIN
+
+# module load cuda-12.1.1
+# LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64
+# module load ISG/experimental/fg12/openmpi/5.0.4-cuda12.1-lsf
+WORKING_DIR=/nfs/users/nfs_j/jc59/quantumwork/pangenome/qiskit_simulation/qiskit_qaoa/cvar
+outdir="$SCRATCH/out/qiskit/cvar"
+
+# Qiskit Testing
+echo "Qiskit Testing"
+bsub -J "optimize_cvar" -R '"select[mem>'$memory'] rusage[mem='$memory']"' -gpu "num=$num_gpu:aff=no" -M "$memory"\
+ -o "$outdir/$filename.%J" -e "$outdir/error.$filename.%J" -G "qpg" -q "qpg" \
+ "python3 $WORKING_DIR/optimize.py -f $filename -p $reps -m $memory -n $shots $hardware"
+
+exit 0
---
+++ qiskit_simulation/bin/plot_cvar.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+usage()
+{
+    echo "usage: plot_cvar.sh [[-f file -m memory -p reps] | [-h]]"
+}
+
+memory=4000
+reps=4
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                filename="$1"
+                                ;;
+        -m | --memory )         shift
+                                memory="$1"
+                                ;;
+        -p | --reps )           shift
+                                reps="$1"
+                                ;;
+        -n | --shots )          shift
+                                shots="$1"
+                                ;;
+        --hardware )            hardware="--hardware"
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+## MAIN
+
+# module load cuda-12.1.1
+# LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64
+# module load ISG/experimental/fg12/openmpi/5.0.4-cuda12.1-lsf
+WORKING_DIR=/nfs/users/nfs_j/jc59/quantumwork/pangenome/qiskit_simulation/qiskit_qaoa/cvar
+outdir="$SCRATCH/out/qiskit/cvar"
+
+# Qiskit Testing
+echo "Qiskit Testing"
+bsub -J "plot_cvar" -R '"select[mem>'$memory'] rusage[mem='$memory']"' -M "$memory"\
+ -o "$outdir/qiskit.$filename.plot.%J" -e "$outdir/error.qiskit.$filename.plot.%J" -G "qpg" -q "normal" \
+ "python3 $WORKING_DIR/plot_cvar.py -f $filename -p $reps -n $shots $hardware"
+
+exit 0
